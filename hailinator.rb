609f1c68e3e3fe471bf14a5d021795cefac8afab
MakerSquare Student <student@challenge-5.att.net>
2013-06-19 09:36:52 -0500

    Added Twitter OAuth

---
+++ hailinator.rb
@@ -0,0 +1,8 @@
+require "twitter"
+
+Twitter.configure do |config|
+  config.consumer_key = "f7Kkx1x1M1TcvBmgLALl1g"
+  config.consumer_secret = "A7Ixe8QC4OdozQeK33SukptijllyMACuwWyW74j4"
+  config.oauth_token = "14859518-xMT4AWraObBx4tbc7llsR47gQI0UO9oMymEaTBGO5"
+  config.oauth_token_secret = "0dWJipCTBpaKPz1SH2ozTe6hFVz3eti7xuxs3Nw2ck"
+end
