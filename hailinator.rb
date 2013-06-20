819d00c9e6b4de3cc5fc9f892d3a7e5bdbb49b05
MakerSquare Student <student@challenge-5.att.net>
2013-06-20 10:29:46 -0500

    Iterated through tweets, added to csv, refactored

--- hailinator.rb
+++ hailinator.rb
@@ -2,36 +2,24 @@ require "twitter"
 require "csv"
 require 'fileutils'
 
-Twitter.configure do |config|
+Twitter.configure do |config| # AUTHENTICATION
   config.consumer_key = "f7Kkx1x1M1TcvBmgLALl1g"
   config.consumer_secret = "A7Ixe8QC4OdozQeK33SukptijllyMACuwWyW74j4"
   config.oauth_token = "14859518-xMT4AWraObBx4tbc7llsR47gQI0UO9oMymEaTBGO5"
   config.oauth_token_secret = "0dWJipCTBpaKPz1SH2ozTe6hFVz3eti7xuxs3Nw2ck"
 end
 
-# result = Twitter.search("hi")
-
-
-# Twitter.search("hi", :count => 3, :result_type => "recent").results.map do |status|
-#   puts "#{status.from_user}: #{status.text}"
-# end
-
-
-puts Twitter.search("hi", :count => 3, :result_type => "recent").results.inspect
-
+puts Twitter.search("hi", :count => 3, :result_type => "recent").results.inspect # TWITTER::TWEETS Class
 
 FileUtils.touch('file.csv')
 
-#Hacked Code#
-# CSV.open("file.csv", "wb") do |csv|
-#   csv << ["handle", "text", "url"]
-#   csv << ["@food", "hello", "www.hood.com"]
-#   csv << ["@food", "hello", "www.hood.com"]
-#   csv << ["@food", "hello", "www.hood.com"]
-# end
-
-#Refactored Code#
 results = Twitter.search("hi", :count => 3, :result_type => "recent").results
-results.map do | status |
-	puts "#{status.from_user}: #{status.text}: user.url" #get inside each tweet object and get handle, text, url
+
+
+CSV.open("file.csv", "wb") do |csv| # OPEN CSV
+	csv << ["handle", "text", "url"] # CREATE HEADERS
+	results.each do | status |
+		puts "#{status.from_user}: #{status.text}: http://www.twitter.com/#{status.from_user}/status/#{status.id}" #get inside each tweet object and get handle, text, url
+		csv << ["#{status.from_user}", "#{status.text}, http://www.twitter.com/#{status.from_user}/status/#{status.id}"]
+	end
 end
