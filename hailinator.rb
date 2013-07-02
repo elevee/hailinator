34ffd7e19c005d321f525a78aac211a3a7df9279
Tim Preston <134735@gmail.com>
2013-07-02 18:43:50 -0500

    Recommitted with foreman

--- hailinator.rb
+++ hailinator.rb
@@ -3,10 +3,10 @@ require "csv"
 require 'fileutils'
 
 Twitter.configure do |config| # AUTHENTICATION
-  config.consumer_key = "f7Kkx1x1M1TcvBmgLALl1g"
-  config.consumer_secret = "A7Ixe8QC4OdozQeK33SukptijllyMACuwWyW74j4"
-  config.oauth_token = "14859518-xMT4AWraObBx4tbc7llsR47gQI0UO9oMymEaTBGO5"
-  config.oauth_token_secret = "0dWJipCTBpaKPz1SH2ozTe6hFVz3eti7xuxs3Nw2ck"
+  config.consumer_key = ENV[CONSUMER_KEY]
+  config.consumer_secret = ENV[CONSUMER_SECRET]
+  config.oauth_token = ENV[OAUTH_TOKEN]
+  config.oauth_token_secret = ENV[OAUTH_TOKEN]
 end
 
 puts Twitter.search("hi", :count => 3, :result_type => "recent").results.inspect # TWITTER::TWEETS Class
